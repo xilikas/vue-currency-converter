33c299d5d6becdb30be72be855d387a27983b0f5
Fil Luzadas <flg.l@outlook.com>
2019-03-30 22:43:48 -0400

    Computed output from initialCurrency field

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,36 @@
 <template lang="pug">
   div#app
     h1  Currency Converter
+    input(type="text" id="initial" placeholder="0" v-model="initialCurrency")
+    label(for="initial") CAD = 
+    input(type="text" id="convert" placeholder="0" v-model="convertCurrency")
+    label(for="convert") USD
+
 </template>
+
+<script lang="ts">
+import { Component, Vue } from 'vue-property-decorator';
+
+@Component({
+  data() {
+    return {
+      initialCurrency: null,
+      convertCurrency: null,
+    }
+  },
+  computed: {
+    initToConv: function () {
+      let { convertCurrency } = this.$data;
+      const initialCurrency = Number.parseInt(this.$data.initialCurrency);
+      this.$data.convertCurrency = +(this.$data.initialCurrency * 0.75).toFixed(2);
+      return this.$data.convertCurrency;
+    }
+  }
+})
+export default class App extends Vue {
+}
+</script>
+
 
 <style lang="scss">
 #app {
